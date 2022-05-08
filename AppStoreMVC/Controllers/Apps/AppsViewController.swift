55262605f615b7a6ee8645d8dcf92c068b2b952f
simransdsu <simran5590@gmail.com>
2022-05-08 23:18:23 +0530

    UICollectionView with default cells of full width and 250 height.

--- AppStoreMVC/Controllers/Apps/AppsViewController.swift
+++ AppStoreMVC/Controllers/Apps/AppsViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class AppsViewController: BaseTabHostViewController {
 
+    private let cellId = "\(UICollectionViewCell.self)"
+    
     private var collectionView: UICollectionView = {
         let flowLayout = UICollectionViewFlowLayout()
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
@@ -22,6 +24,7 @@ class AppsViewController: BaseTabHostViewController {
         super.viewDidLoad()
         style()
         layout()
+        setup()
     }
     
     private func layout() {
@@ -30,17 +33,59 @@ class AppsViewController: BaseTabHostViewController {
     }
     
     private func style() { }
+    
+    private func setup() {
+        
+        setupCollectionView()
+    }
 }
 
 // MARK: - UIViews setup
 private extension AppsViewController {
     
-    private func layoutCollectionView() {
+    func layoutCollectionView() {
         
         view.addSubview(collectionView)
         collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+    
+    func setupCollectionView() {
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+}
+
+
+// MARK: - CollectionView Data Source
+extension AppsViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 5
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+
+// MARK: - CollectionView Delegate Flow Layout
+extension AppsViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return .init(width: view.frame.width, height: 250)
     }
 }
