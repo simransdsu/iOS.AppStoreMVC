ce2ab2461922ff0a5647e7bec225848c080a13df
simransdsu <simran5590@gmail.com>
2022-05-08 23:10:05 +0530

    Layout CollectionView in AppsViewController

--- AppStoreMVC/Controllers/Apps/AppsViewController.swift
+++ AppStoreMVC/Controllers/Apps/AppsViewController.swift
@@ -9,7 +9,38 @@ import UIKit
 
 class AppsViewController: BaseTabHostViewController {
 
+    private var collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemBackground
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        style()
+        layout()
+    }
+    
+    private func layout() {
+        
+        layoutCollectionView()
+    }
+    
+    private func style() { }
+}
+
+// MARK: - UIViews setup
+private extension AppsViewController {
+    
+    private func layoutCollectionView() {
+        
+        view.addSubview(collectionView)
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
 }
