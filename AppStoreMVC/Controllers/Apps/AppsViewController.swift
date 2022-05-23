47ccc667203512864307e33b818aed389b431869
simransdsu <simran5590@gmail.com>
2022-05-23 21:03:07 +0530

    Created a place holder collection view in AppsViewController

--- AppStoreMVC/Controllers/Apps/AppsViewController.swift
+++ AppStoreMVC/Controllers/Apps/AppsViewController.swift
@@ -9,8 +9,95 @@ import UIKit
 
 class AppsViewController: BaseTabHostViewController {
 
+    private let cellId = "\(UICollectionViewCell.self)"
+    private var dataSource = [String]() {
+        didSet {
+            DispatchQueue.main.async { [weak self] in
+                self?.collectionView.reloadData()
+            }
+        }
+    }
+    
+    private var collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemBackground
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        style()
+        layout()
+        setup()
+        
+    }
+    
+    private func layout() {
+        
+        layoutCollectionView()
+    }
+    
+    private func style() {
+        
+    }
+    
+    private func setup() {
+        
+        setupCollectionView()
+        dataSource.append(contentsOf: ["1", "2", "3", "4", "5", "6"])
     }
 }
 
+
+// MARK: - UIViews setup
+private extension AppsViewController {
+    
+    func layoutCollectionView() {
+        
+        view.addSubview(collectionView)
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+    
+    func setupCollectionView() {
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self.self, forCellWithReuseIdentifier: cellId)
+    }
+}
+
+
+// MARK: - CollectionView Data Source
+extension AppsViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return dataSource.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .systemGray6
+        return cell
+    }
+}
+
+
+// MARK: - CollectionView Delegate Flow Layout
+extension AppsViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        
+        return .init(width: view.frame.width, height: 250)
+    }
+}
