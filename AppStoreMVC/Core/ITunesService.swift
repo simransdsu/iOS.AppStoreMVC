8b3e964c38583e5f5b5fa94ca86315c2db322ed5
Simran Preet Singh <simran5590@gmail.com>
2022-05-26 07:27:19 -0700

    removed multiple uses of `addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)` to a one single place

--- AppStoreMVC/Core/ITunesService.swift
+++ AppStoreMVC/Core/ITunesService.swift
@@ -14,38 +14,38 @@ class ITunesService {
     static let shared = ITunesService()
     
     func searchAPI(withTerm term: String) async throws -> SearchResultAPI {
-        let urlString = "https://itunes.apple.com/search?term=\(term)&entity=software".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        let urlString = "https://itunes.apple.com/search?term=\(term)&entity=software"
         return try await call(endpoint: urlString)
     }
     
     func fetchTopApps() async throws -> SearchResultAPI {
         
-        let urlString = "https://itunes.apple.com/search?term=top apps&entity=software".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        let urlString = "https://itunes.apple.com/search?term=top apps&entity=software"
         return try await call(endpoint: urlString)
     }
     
     func fetchTopProductivityApps() async throws -> SearchResultAPI {
         
-        let urlString = "https://itunes.apple.com/search?term=productivity&entity=software".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        let urlString = "https://itunes.apple.com/search?term=productivity&entity=software"
         return try await call(endpoint: urlString)
     }
     
     func fetchTopUtilityApps() async throws -> SearchResultAPI {
         
-        let urlString = "https://itunes.apple.com/search?term=utility&entity=software".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        let urlString = "https://itunes.apple.com/search?term=utility&entity=software"
         return try await call(endpoint: urlString)
     }
     
     func fetchHeaders() async throws -> [Header] {
         
-        let urlString = "https://api.letsbuildthatapp.com/appstore/social".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        let urlString = "https://api.letsbuildthatapp.com/appstore/social"
         return try await call(endpoint: urlString)
     }
     
     
     private func call<T: Decodable>(endpoint: String) async throws -> T {
         
-        guard let url = URL(string: endpoint) else {
+        guard let url = URL(string: endpoint.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "") else {
             throw APIError.invalidUrl("❌ \(endpoint) is invalid")
         }
         
